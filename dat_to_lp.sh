a550058a5f251402098e2827b527f2ee88185b6e
Romerito Campos <romerito.campos@gmail.com>
2015-03-01 14:11:04 -0300

    dat_to_lp generates a file compressed by gzip

--- dat_to_lp.sh
+++ dat_to_lp.sh
@@ -6,12 +6,18 @@ data_instances=$1
 
 output=$2
 
+#o binário está no projeto de doutorado LpGenerator
+bin=$3
+
 for inst in `ls ${data_instances}`
 do
 
 	file=`basename ${inst} .dat`
 
 	#cria arquivos *.lp para servir de entrada no gurobi
-	glpsol --check --wlp ${output}/${file}.lp --math src/mmmstp.mod --data ${data_instances}/${inst}
+	#glpsol --check --wlp ${output}/${file}.lp --math src/mmmstp.mod --data ${data_instances}/${inst}
+	${bin} ${data_instances}/${inst} > ${output}/${file}.lp
+	gzip ${output}/${file}.lp
+	rm ${output}/${file}.lp
 
 done
