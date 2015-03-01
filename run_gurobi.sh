deb6423c8696df6e51cd89eb61a683ff3fed7d1f
Romerito Campos <romerito.campos@gmail.com>
2015-03-01 15:16:26 -0300

    run_gurobi script modified

--- run_gurobi.sh
+++ run_gurobi.sh
@@ -9,7 +9,7 @@ output=$2
 
 for inst in `ls ${gurobi_instances}`
 do
-	file=`basename ${inst} .lp`
+	file=`basename ${inst} .lp.gz`
 
 	#executa gurobi gerando a solução e informações de execução
 	gurobi_cl ResultFile=${output}/${file}.sol ${gurobi_instances}/${inst} > ${output}/${file}.info
