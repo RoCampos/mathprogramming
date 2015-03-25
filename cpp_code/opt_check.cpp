977ab622b9e9ec68b62fa4c3345b4a556af22a98
Romerito Campos <romerito.campos@gmail.com>
2015-03-25 15:02:32 -0300

    Bug fixed

--- cpp_code/opt_check.cpp
+++ cpp_code/opt_check.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <libgen.h>
+#include <cstdlib>
 
 using namespace std;
 
@@ -16,18 +17,44 @@ int main (int argv, char**argc)
 	//getting the instance
 	char* inst = basename (argc[1]);
 
-	float best, bound;
-	float gap;
-	while ( getline (file, str) ) { //search for line where the result is
+	float best, bound, time, gap;
 
+	int nodes, exp;
+
+	while ( getline (file, str) ) { //search for line where the result is
 		//testing the matching with result
-		sscanf (str.c_str(), "Best objective %f, best bound %f, gap %f%",
+
+		string st(str);		
+		sscanf (str.c_str(), "Best objective %f, best bound %f, gap %f",
 				&best,&bound,&gap);
+	
+
+		sscanf (st.c_str(), "Explored %d nodes (%d simplex iterations) in %f seconds",
+				&nodes, &exp, &time);
+
 	}
 
+	printf ("%s\t%d\t%d\t%.2f\t%.2lf\n",inst,(int)best,(int)bound, gap, time);
+
+	file.close ();
+
+	file.open (argc[1]);
+
+	if (!file.good () ) {
+		exit(1);	
+	}
+
+/*
+	ifstream file2 (argc[1]);
+
+	std::cout << "here" << std::endl;
+	while ( getline (file1, str)) {
+		sscanf (str.c_str(), "Explored %d nodes (%d simplex iterations) in %f seconds",
+				&nodes, &exp, time);
+	}
 	//printing out the result
-	printf ("%s\t%d\t%d\t%.2f\n",inst,(int)best,(int)bound,gap);
-		
+	printf ("%s\t%d\t%d\t%.2f\t%.2lf\n",inst,(int)best,(int)bound,gap, time);
+*/		
 
 	return 0;
 }
